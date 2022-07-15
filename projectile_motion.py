da2be1325c08b95da642c2c7302de4acc0fdcc18
AldairBryan <asalcedoch@unsa.edu.pe>
2022-07-14 19:32:48 -0500

    Comentarios agregados

--- projectile_motion.py
+++ projectile_motion.py
@@ -8,14 +8,13 @@ hScreen = 500
 #Info del Nivel
 nivel=1
 gravedad=9.8
-power_act=0
-forze_act=0
 
 #Inicializa
 win = pygame.display.set_mode((wScreen,hScreen))
 pygame.display.set_caption('Projectile Motion')
 pygame.font.init()
 
+#Fuentes Para los Textos
 font_coordenadas = pygame.font.SysFont('Comic Sans MS', 23)
 font_info = pygame.font.SysFont('Comic Sans MS', 20)
 
@@ -26,7 +25,8 @@ class ball(object):
         self.y = y
         self.radius = radius
         self.color = color
-
+    
+    #Pelota
     def draw(self, win):
         pygame.draw.circle(win, (0,0,0), (self.x,self.y), self.radius)
         pygame.draw.circle(win, self.color, (self.x,self.y), self.radius-1)
@@ -45,28 +45,26 @@ class ball(object):
         distX = velx * time
         distY = (vely * time) + ((-gravedad * (time ** 2)) / 2)  #Gravedad
 
+        #Nuevas Posiciones
         newx = round(distX + startx)
         newy = round(starty - distY)
-        trajectoryLaunch.append(newx)
-        trajectoryLaunch.append(newy)
         return (newx, newy)
 
-#Mostrar Posicion
-
-#Iniciar
+#Guarda los puntos del recorrdio
 trajectoryLaunch=[]
 
+#Iniciar
 def redrawWindow(shooted):
 
     win.fill((64,64,64))
     golfBall.draw(win)
-    pygame.draw.line(win, (0,0,0),line[0], line[1])
-    drawInformation()
-    drawParabol()
+    pygame.draw.line(win, (0,0,0),line[0], line[1])     #Linea del mouse
+    drawInformation()   #Mostrar Informacion del Nivel
+    drawParabol()       #Mostrar El movimiento Parabolico
     pygame.display.update()
 
 def drawInformation():
-
+    #Informacion del NIvel
     text_info=font_info.render('Nivel: '+str(nivel),False,(0,255,0))
     win.blit(text_info, (20,20))
     text_info=font_info.render('Gravedad: '+str(gravedad),False,(0,255,0))
@@ -75,10 +73,11 @@ def drawInformation():
     win.blit(text_info, (20,80))
     text_info=font_info.render('Fuerza: '+str(power_act),False,(0,255,0))
     win.blit(text_info, (20,110))
-
+    #Informacion de la posicion
     text_surface = font_coordenadas.render('X:'+str(line[0][0])+'  Y: '+str(line[0][1]), False, (0, 0, 0))
     win.blit(text_surface, (line[0][0],line[0][1]-50))
 
+#Muestra la Parabola
 def drawParabol():
     for i in range(int(len(trajectoryLaunch)/4)):
         x1=trajectoryLaunch[(i*4)]
@@ -117,26 +116,31 @@ power = 0
 angle = 0
 shoot = False
 clock = pygame.time.Clock()
+
 while run:
-    clock.tick(200)
+    clock.tick(200)     #Reloj
+    #Informacion actual
     angle_act=findAngle(pygame.mouse.get_pos())
     line_act = [(golfBall.x, golfBall.y), pygame.mouse.get_pos()]
     power_act=math.sqrt((line_act[1][1]-line_act[0][1])**2 +(line_act[1][0]-line_act[0][1])**2)/5
+
+    #Cuando la pelota haya sido disparada y este en el recorrido
     if shoot:
         if golfBall.y < 500 - golfBall.radius:
-            trajectoryLaunch.append(golfBall.x)
+            trajectoryLaunch.append(golfBall.x)     #Agregamos posicion actual
             trajectoryLaunch.append(golfBall.y)
             time += 0.05
-            po = ball.ballPath(x, y, power, angle, time)
+            po = ball.ballPath(x, y, power, angle, time)    #Recorrido
             golfBall.x = po[0]
             golfBall.y = po[1]
-            trajectoryLaunch.append(golfBall.x)
+            trajectoryLaunch.append(golfBall.x)     #Agregamos posicion nueva
             trajectoryLaunch.append(golfBall.y)
-        else:
+        else:       #Aterrizo
             shoot = False
             time = 0
             golfBall.y = 494
 
+    #Linea al mouse
     line = [(golfBall.x, golfBall.y), pygame.mouse.get_pos()]
     redrawWindow(shoot)
 
@@ -144,6 +148,7 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+        #Lanza
         if event.type == pygame.MOUSEBUTTONDOWN:
             if not shoot:
                 x = golfBall.x
